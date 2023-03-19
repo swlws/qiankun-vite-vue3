307d1536778a14954a61765d8f593a756f5d8b80
swlws <979623074@qq.com>
2023-03-19 14:59:36 +0800

    feat: html

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 root=$(pwd)
 echo $root
 
-rm -rf $root/docs
+rm -rf $root/html
 
 cd $root/main-app && pnpm build
 cd $root/app-01 && pnpm build
 cd $root/app-02 && pnpm build
 
-mkdir -p $root/docs/sub/
+mkdir -p $root/html/sub/
 
-mv $root/main-app/dist/* $root/docs
-mv $root/app-01/dist $root/docs/sub/app-01
-mv $root/app-02/dist $root/docs/sub/app-02
+mv $root/main-app/dist/* $root/html
+mv $root/app-01/dist $root/html/sub/app-01
+mv $root/app-02/dist $root/html/sub/app-02
